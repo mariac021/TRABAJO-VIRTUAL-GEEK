9b41366bee8a0ead9541a38591e61cbc77798ab1
mariac021 <maclarea021@gmail.com>
2020-03-16 23:32:14 -0500

    tercer cambio

--- 2-PROGRAMACION-DOS/retoDos/retoDos.js
+++ 2-PROGRAMACION-DOS/retoDos/retoDos.js
@@ -1,6 +1,4 @@
 'use strict';
 
 const superWave = document.querySelector('.super-wave');
-const content = '<h1 class=main__title2>OLA!</h1>';
-
-containerElement.innerHTML = content;
+superWave.classList.add('super-border');
